3ccdfcfcd2bc02d976c35b8c769ad3c005c31958
Alexsei Popov <popov2895@gmail.com>
2023-11-07 00:51:22 +0300

    change CORS policy

--- controller/__init__.py
+++ controller/__init__.py
@@ -4,17 +4,19 @@ import auth_service.authservice_pb2_grpc as pb2_grpc
 import grpc
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
 
+
+load_dotenv()
 CORS_ORIGIN = [
-    "http://localhost:3000",
-    "http://localhost:5000",
+    "*"
 ]
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True, origins=CORS_ORIGIN)
 
 auth_service_channel = grpc.insecure_channel(
-    f"localhost:{os.getenv('AUTH_SERVICE_PORT')}"
+    f"{os.getenv('AUTH_SERVICE_HOST')}:{os.getenv('AUTH_SERVICE_PORT')}"
 )
 auth_service_stub = pb2_grpc.AuthServiceStub(auth_service_channel)
 
