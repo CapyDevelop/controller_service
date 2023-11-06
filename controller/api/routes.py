c34204dd7fdb2847d8afa06ba0eef044088d67dd
Alexsei Popov <popov2895@gmail.com>
2023-11-06 21:02:38 +0300

    comment unused lines

--- controller/api/routes.py
+++ controller/api/routes.py
@@ -1,11 +1,11 @@
-import school_service.school_service_pb2 as school_pb2
-
-from controller import auth_service_stub
-
-from . import api
-
-
-@api.get("/get_user_data")
-def get_user_data():
-    pass
-
+# import school_service.school_service_pb2 as school_pb2
+#
+# from controller import auth_service_stub
+#
+# from . import api
+#
+#
+# @api.get("/get_user_data")
+# def get_user_data():
+#     pass
+#
